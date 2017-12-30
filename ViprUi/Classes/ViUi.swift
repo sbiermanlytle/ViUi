151f999437f12f8b18a368a4761c745a7a1be003
Sebastian Bierman-Lytle <sbiermanlytle@gmail.com>
2017-12-30 12:53:41 -0800

    ibid

--- ViprUi/Classes/ViUi.swift
+++ ViprUi/Classes/ViUi.swift
@@ -12,9 +12,6 @@ public struct ViUi {
     // MARK: Fonts
     // --------------------------------------------------------------------------
     public static func font(_ size: CGFloat) -> UIFont {
-        if #available(iOS 8.2, *) {
-            return UIFont.systemFont(ofSize: size, weight: .bold)
-        }
         return UIFont(name: "Helvetica", size: size)!
     }
 }
